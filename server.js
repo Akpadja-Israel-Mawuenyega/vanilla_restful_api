b67c7220e30c4ef1fcacc387898232ce8e06f10e
Israel Akpadja <drimmaculate147@gmail.com>
2024-10-08 22:48:29 +0000

    committed

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const http = require('http')
-const PORT = process.env
+const http = require("http");
+const { getProducts } = require("./controllers/productController");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify(products))
-})
+  if (req.url === "/api/products" && req.method === "GET") {
+    getProducts(req, res);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ Message: "Route not found!" }));
+  }
+});
 
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
